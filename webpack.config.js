ec59e11d820875477ac7b082add84a0dd0f3c8ae
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-10-19 23:39:17 -0500

    added html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 
+
+
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
   mode: "development",
@@ -10,4 +14,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
+
+
+
